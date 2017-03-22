ab904c014bd120ed050f96f29d8b71b572e2c075
trikitrok <trikitrok@gmail.com>
2017-03-22 20:07:03 +0100

    refactor: extracting explaining helper function

--- src/luhn_test/core.clj
+++ src/luhn_test/core.clj
@@ -2,10 +2,13 @@
 
 (def ^:private sum-digits #(+ (quot % 10) (mod % 10)))
 
+(defn- double-when-at-even-position [position num]
+  (if (even? position) num (* 2 num)))
+
 (defn- reduce-numbers [numbers]
   (apply +
          (map sum-digits
-           (map-indexed #(if (even? %1) %2 (* 2 %2)) numbers))))
+           (map-indexed double-when-at-even-position numbers))))
 
 (defn valid? [digits]
   (let [numbers (map #(Integer/parseInt (str %)) (reverse digits))]
