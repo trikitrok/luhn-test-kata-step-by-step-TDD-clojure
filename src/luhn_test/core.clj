ad181579abf0ed332bad06f59db7a672b2f862ac
trikitrok <trikitrok@gmail.com>
2017-03-22 20:04:28 +0100

    refactor: extracting sum-of-digits responsibility

--- src/luhn_test/core.clj
+++ src/luhn_test/core.clj
@@ -1,8 +1,10 @@
 (ns luhn-test.core)
 
+(def ^:private sum-digits #(+ (quot % 10) (mod % 10)))
+
 (defn- reduce-numbers [numbers]
   (apply +
-         (map #(+ (quot % 10) (mod % 10))
+         (map sum-digits
            (map-indexed #(if (even? %1) %2 (* 2 %2)) numbers))))
 
 (defn valid? [digits]
