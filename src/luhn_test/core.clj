82a39feb001e192cff87d39d03cbd405de8e1f53
trikitrok <trikitrok@gmail.com>
2017-03-22 19:44:39 +0100

    refactor: extracting numbers reduction and ordering expression to make the pattern of repetition more apparent

--- src/luhn_test/core.clj
+++ src/luhn_test/core.clj
@@ -1,10 +1,13 @@
 (ns luhn-test.core)
 
+(defn- reduce-numbers [numbers]
+  (+ (first numbers)
+     (* 2 (second numbers))
+     (get (vec numbers) 2 0)
+     (* 2 (get (vec numbers) 3 0))))
+
 (defn valid? [digits]
   (let [numbers (map #(Integer/parseInt (str %)) (reverse digits))]
     (if (= 1 (count numbers))
       (zero? (first numbers))
-      (= 10 (+ (first numbers)
-               (get (vec numbers) 2 0)
-               (* 2 (second numbers))
-               (* 2 (get (vec numbers) 3 0)))))))
+      (= 10 (reduce-numbers numbers)))))
