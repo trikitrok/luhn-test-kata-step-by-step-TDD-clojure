5c4cdafe4f659e35d215604eadbfab147170152f
trikitrok <trikitrok@gmail.com>
2017-03-22 19:15:08 +0100

    10 pass the Luhn test

--- src/luhn_test/core.clj
+++ src/luhn_test/core.clj
@@ -1,4 +1,7 @@
 (ns luhn-test.core)
 
 (defn valid? [digits]
-  (zero? (Integer/parseInt digits)))
+  (if (= 1 (count digits))
+    (zero? (Integer/parseInt digits))
+    (= 10 (+ (Integer/parseInt (str (second digits)))
+             (* 2 (Integer/parseInt (str (first digits))))))))
--- test/luhn_test/core_test.clj
+++ test/luhn_test/core_test.clj
@@ -5,4 +5,5 @@
 (facts
   "about luhn test"
   (luhn/valid? "0") => true
-  (luhn/valid? "1") => false)
+  (luhn/valid? "1") => false
+  (luhn/valid? "18") => true)
